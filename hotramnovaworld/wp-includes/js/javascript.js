b014d81160ec3d864868691b4e0e8340b45cdec1
Vinh Ngo <nanyangbk@gmail.com>
2022-05-07 14:28:12 +0700

    Update popup

--- hotramnovaworld/wp-includes/js/javascript.js
+++ hotramnovaworld/wp-includes/js/javascript.js
@@ -1,5 +1,39 @@
 
-setTimeout(function () {
-    console.log('modal')
+setInterval(function () {
     $('#exampleModalLong').modal();
-}, 2000);
+}, 20000);
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyzF4Tq4lvPHyDFhmMdYoSUJUbU-uqdqXTl7DGIa0YZLR_bkZjNk277EnlcoBO4QaBm/exec'
+const form = document.forms['google-sheet']
+
+const forms = document.forms
+
+for (const item in forms) {
+    forms[item].addEventListener('submit', e => {
+
+        e.preventDefault()
+        fetch(scriptURL, {
+            method: 'POST', body: new FormData(forms[item])
+        })
+            .then(response => {
+                $('#tksForm').modal();
+                $('#exampleModalLong').modal('hide');
+             })
+            .catch(error => console.error('Error!', error.message))
+    })
+}
+
+console.log('form', document.forms)
+
+form.addEventListener('submit', e => {
+    console.log(e)
+    e.preventDefault()
+    fetch(scriptURL, {
+        method: 'POST', body: new FormData(form)
+    })
+    .then(response => {
+        $('#tksForm').modal();
+        $('#exampleModalLong').modal('hide');
+     })
+    .catch(error => console.error('Error!', error.message))
+})
